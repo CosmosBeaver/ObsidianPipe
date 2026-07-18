f8896871fbb532a532fa1ad77979a0e3071c889c
Mihai <mihaiadrian.mitea2006@gmail.com>
2026-07-18 15:25:12 +0300

    enabled --input and --output commands when running main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,36 @@
 import os
+import argparse
+from datetime import datetime
 import transformers.models.vision_encoder_decoder.modeling_vision_encoder_decoder as ved
+from orchestrator import run_pipeline
+
+input_dir = "a"
+output_dir = "b"
+
+def parse_config():
+    parser = argparse.ArgumentParser(description="Obsidian Vault Generator")
+    
+    #define the arguments
+    parser.add_argument(
+        '--input',
+        type=str,
+        help="Path to the input directory"
+    )
+    parser.add_argument(
+        '--output',
+        type=str,
+        help="Path to the output directory"
+    )
+    
+    args = parser.parse_args()
+    
+    # Priority 1: CLI arguments
+    # Priority 2: Environment variables (Great for Docker)
+    # Priority 3: Fallback defaults
+    input_dir = args.input or os.getenv('INPUT_DIR') or '/ObsidianPipe/input_files'
+    output_dir = args.output or os.getenv('OUTPUT_DIR') or '/ObsidianPipe/test_output'
+    
+    return input_dir, output_dir
 
 # --- SURGICAL RUNTIME PATCH FOR MINERU ---
 # This intercepts the model during execution and drops the 'cache_position' 
@@ -19,28 +50,27 @@ def patched_ved_forward(self, *args, **kwargs):
 
 ved.VisionEncoderDecoderModel.forward = patched_ved_forward
 # -----------------------------------------
-from orchestrator import run_pipeline
-from datetime import datetime
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-INPUT_DIR = os.path.join(BASE_DIR, "input_files")
-
-#mihai modifica si tu ca mi e lene sa implementez altceva inafara de hardcodare
-OUTPUT_DIR = os.path.join(BASE_DIR, "ObsidianVault")
-def main():
+def main(input_dir, output_dir):
     print("========================================")
     print(" Starting Obsidian Vault Generation...  ")
     print("========================================")
     
-    if not os.path.exists(INPUT_DIR):
-        print(f"Error: Input directory not found at {INPUT_DIR}")
+    if not os.path.exists(input_dir):
+        print(f"Error: Input directory not found at {input_dir}")
         return
 
     # Orchestrator
-    run_pipeline(INPUT_DIR, OUTPUT_DIR)
+    run_pipeline(input_dir, output_dir)
 
 if __name__ == "__main__":
     start=datetime.now()
-    main()
-    end=datetime.now()
+    
+    # Grab the arguments from the terminal or Docker environment
+    dynamic_input, dynamic_output = parse_config()
+    
+    # Feed those arguments into the main function
+    main(dynamic_input, dynamic_output)
+    
+    end = datetime.now()
     print(f"time: {end-start}")
